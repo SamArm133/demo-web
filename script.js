25a53ea5919c029417de24f54e6ffbc432ff11b8
Samuel Salazar <samuel.salazar1333@gmail.com>
2020-09-26 11:02:54 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function myfunction(){
+	document.getElementById("demo").innerHTML = "Hello";
+}
+var Date = new Date();
+    document.getElementById("am").innerHTML = "Date";
+
+alert("Welcome");
+
